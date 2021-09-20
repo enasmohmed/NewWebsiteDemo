7ce2c76ca4cc6c0eed2bfb688857f0e01c6b25db
Enas Mohamed <enasm2477@gmail.com>
2021-09-20 20:55:02 +0200

    Add Social Media Sidebar and Add Button Video

--- assets/js/main.js
+++ assets/js/main.js
@@ -117,3 +117,22 @@ $( document ).ready(function() {
     });
 
 
+
+$('#play-video').on('click', function(e){
+  e.preventDefault();
+  $('#video-overlay').addClass('open');
+  $("#video-overlay").append('<iframe width="560" height="315" src="https://drive.google.com/file/d/1rI8O3tKUM3jSpbq1SiB9yqqNL67801yr/view?usp=sharing" frameborder="0" allowfullscreen></iframe>');
+});
+
+$('.video-overlay, .video-overlay-close').on('click', function(e){
+  e.preventDefault();
+  close_video();
+});
+
+$(document).keyup(function(e){
+  if(e.keyCode === 27) { close_video(); }
+});
+
+function close_video() {
+  $('.video-overlay.open').removeClass('open').find('iframe').remove();
+};
