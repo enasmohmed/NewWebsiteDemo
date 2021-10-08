faca5a30c1df8f6cb8116701ef47379f3a235f35
enasmohmed <enasm2477@gmail.com>
2021-10-09 01:58:55 +0200

    new demo

--- assets/js/main.js
+++ assets/js/main.js
@@ -78,7 +78,7 @@ $( document ).ready(function() {
   // Slider 
       $('.home-slider').owlCarousel({
       loop:true,
-      autoplay: true,
+      autoplay: false,
       margin:0,
       animateOut: 'fadeOut',
       animateIn: 'fadeIn',
@@ -169,3 +169,43 @@ $( document ).ready(function() {
   });
 
 
+
+
+var aText = new Array(
+"Dox Solutions Cloud Programs to manage all business",
+"and control provisions on their resources from",
+"anywhere and at any time."
+);
+var iSpeed = 50; // time delay of print out
+var iIndex = 0; // start printing array at this posision
+var iArrLength = aText[0].length; // the length of the text array
+var iScrollAt = 20; // start scrolling up at this many lines
+ 
+var iTextPos = 0; // initialise text position
+var sContents = ''; // initialise contents variable
+var iRow; // initialise current row
+ 
+function typewriter()
+{
+ sContents =  ' ';
+ iRow = Math.max(0, iIndex-iScrollAt);
+ var destination = document.getElementById("typedtext");
+ 
+ while ( iRow < iIndex ) {
+  sContents += aText[iRow++] + '<br />';
+ }
+ destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
+ if ( iTextPos++ == iArrLength ) {
+  iTextPos = 0;
+  iIndex++;
+  if ( iIndex != aText.length ) {
+   iArrLength = aText[iIndex].length;
+   setTimeout("typewriter()", 500);
+  }
+ } else {
+  setTimeout("typewriter()", iSpeed);
+ }
+}
+
+
+typewriter();
