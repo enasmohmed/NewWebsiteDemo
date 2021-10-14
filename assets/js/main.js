93aa11583fd1401f8e3b188c150bd0f4aab264e5
enasmohmed <enasm2477@gmail.com>
2021-10-14 13:18:33 +0200

    finish design home page

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,6 +18,26 @@
     }
   });
 
+var swiper = new Swiper(".swiper-container", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 20,
+    stretch: 0,
+    depth: 350,
+    modifier: 1,
+    slideShadows: true
+  },
+  autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination"
+  }
+});
 
 
 /*
