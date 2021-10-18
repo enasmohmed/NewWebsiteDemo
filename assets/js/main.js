3e472a5c09fc3b8e0ff0c6b6be796013f340a68c
enasmohmed <enasm2477@gmail.com>
2021-10-18 21:44:53 +0200

    Add New Design Slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -237,4 +237,45 @@ var mySwiper = new Swiper ('.program__units', {
  
 
 /** Test Slider Header One Section **/
- 
+ $(document).ready(function () {
+  var swiper = new Swiper(".swiper-header", {
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    effect: 'creative',
+    speed: 800,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      type: "fraction"
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    },
+    on: {
+      init: function () {
+        $(".swiper-progress-bar").removeClass("animate");
+        $(".swiper-progress-bar").removeClass("active");
+        $(".swiper-progress-bar").eq(0).addClass("animate");
+        $(".swiper-progress-bar").eq(0).addClass("active");
+      },
+      slideChangeTransitionStart: function () {
+        $(".swiper-progress-bar").removeClass("animate");
+        $(".swiper-progress-bar").removeClass("active");
+        $(".swiper-progress-bar").eq(0).addClass("active");
+      },
+      slideChangeTransitionEnd: function () {
+        $(".swiper-progress-bar").eq(0).addClass("animate");
+      }
+    }
+  });
+  $('.swiper-header').hover(function() {
+    swiper.autoplay.stop();
+    $('.swiper-progress-bar').removeClass('animate');
+  }, function(){
+    swiper.autoplay.start();
+    $('.swiper-progress-bar').addClass('animate');
+  });
+});
